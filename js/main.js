77f2919872f8eacbc58c2e08daccbf8d92b78296
jessicaor <oreonevada@gmail.com>
2019-02-11 15:32:06 +0400

    'skull'

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     // code lu uniquement une fois le document chargé
    
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        document.getElementById("navscroll").style.top = "0";
+      } else {
+        document.getElementById("navscroll").style.top = "-50px";
+      }
+    }
 
   });
